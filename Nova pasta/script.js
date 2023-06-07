7c1c33be4cfa58ab716a8ea23e62832e91b787bb
Keviny1234 <128837027+Keviny1234@users.noreply.github.com>
2023-06-07 12:28:28 -0300

    Add files via upload

---
+++ Nova pasta/script.js
@@ -0,0 +1,35 @@
+botaoQuestao1 = document.querySelector("#botao-questao1")
+
+botaoQuestao1.addEventListener("click", function(event){
+    event.preventDefault()
+    let radios = document.getElementsByName("questao1")
+    let gabarito1 = document.getElementById("gabarito1")
+    for (let i = 0, length = radios.length; i < length; i++) {
+        //radios[i].parentElement.classList.remove("certa")
+        //radios[i].parentElement.classList.remove("errada")
+        
+        if (radios[i].checked) {
+            if(radios[i].value == 'B'){
+                gabarito1.innerHTML = "Letra B. Resposta certa <br>   Resolução:  80 = 10.q^(4-1)  <br>  80/10 = q^3 <br>  8 = q^3  <br>  2^3 = q^3  <br>  q = 2 "
+                radios[i].parentElement.classList.add("certa")
+            }else{
+                gabarito1.innerHTML = "Letra B. Resposta errada <br>  Resolução:  80 = 10.q^(4-1)  <br>  80/10 = q^3 <br>  8 = q^3  <br>  2^3 = q^3  <br>  q = 2"
+                radios[i].parentElement.classList.add("errada")
+            }
+            
+            break;
+        }
+    }
+})
+ 
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
